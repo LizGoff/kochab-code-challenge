435ea11900533752c7b682dc27a1a9b9a7d49c77
Liz Goff <livingearthcreations@gmail.com>
2018-04-16 13:53:35 -0500

    button append in console log

--- script.js
+++ script.js
@@ -7,11 +7,20 @@ function onReady() {
 //    console.log("jquery up");
     $("button").on("click", clickHandler) 
     console.log("been clicked");
+    addThingToDom();
     
     function clickHandler() {
-            console.log("was clicked");
+        console.log("was clicked");
+        addThingToDom();
     }
-}
+
+    function addThingToDom() {
+        $("#thingAdd").append("<div>" + $("#thingAdd").val() + "</div>");
+        console.log("thingAdd");
+    }
+
+
+} //end onReady
 
 //div id="thingAdd"
 //p id="thingCounter"
