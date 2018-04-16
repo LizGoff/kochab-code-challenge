7deddf2ba108fb73cfb1324279312a753789c91f
Liz Goff <livingearthcreations@gmail.com>
2018-04-16 13:43:48 -0500

    button workingbeen clicked and was clicked

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-console.log("JS");
+//console.log("JS");
 
 $(document).ready(onReady);
-console.log("doc ready");
+//console.log("doc ready");
 
 function onReady() {
-    console.log("jquery up");
+//    console.log("jquery up");
+    $("button").on("click", clickHandler) 
+    console.log("been clicked");
+    
+    function clickHandler() {
+            console.log("was clicked");
+    }
 }
+
+//div id="thingAdd"
+//p id="thingCounter"
